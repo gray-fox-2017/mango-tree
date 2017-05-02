9e1041de38b58f060aa20e78041b15dc814e569d
sidikriders <sidik.riders@gmail.com>
2017-05-02 19:37:02 +0700

    selesai class mangga

--- mango_tree.js
+++ mango_tree.js
@@ -6,31 +6,71 @@ class MangoTree {
 
   // Initialize a new MangoTree
   constructor() {
+    this.age = 0;
+    this.tinggi = 0;
+    this.maxHeight = 20;
+    this.currentGoodFruit = 0;
+    this.currentBadFruit = 0;
+    this.harvestedFruit = 0;
+    this.totalHarvestedFruit = 0;
+    this.healthStatus = 150;
   }
 
   getAge() {
+    return `Umur Pohon: ${this.age} Tahun`;
   }
   getHeight() {
-
+    return `Tinggi Pohon: ${this.height} Meter`;
   }
   getFruits() {
+    return `Jumlah buah yang sudah dipetik: ${this.harvestedFruit} Buah (${this.currentGoodFruit} good, ${this.currentBadFruit} bad)`;
   }
-  getHealtyStatus() {
+  get healthyStatus() {
+    if (this.healthStatus > 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
-
-  // Get current states here
-
-  // Grow the tree
   grow() {
+    this.age++;
+
+    var tambahTinggi = Math.floor((Math.random()*10)/2);
+    if ((this.tinggi + tambahTinggi) <= this.maxHeight) {
+      this.tinggi+=tambahTinggi;
+    } else {
+      this.tinggi = this.maxHeight;
+    }
+
   }
 
   // Produce some mangoes
   produceMangoes() {
+
+    var faktor = Math.floor((this.tinggi+this.age)*this.healthStatus/100)
+    for (let i=0; i<=faktor; i++) {
+      var mangga = new Mango();
+      if (mangga.grade === 'good') {
+        this.currentGoodFruit++
+      } else if (mangga.grade === 'bad') {
+        this.currentBadFruit++
+      }
+    }
+    this.harvestedFruit = this.currentBadFruit + this.currentGoodFruit;
+
+    this.harvested = `${this.harvestedFruit} (${this.currentGoodFruit} Good ${this.currentBadFruit} Bad)`
+    return faktor + 'buah';
   }
 
   // Get some fruits
   harvest() {
+    this.totalHarvestedFruit+=this.harvestedFruit;
+    var pengurangHealth = (1*this.currentGoodFruit)+(3*this.currentBadFruit);
+    this.healthStatus -= pengurangHealth;
+    this.currentBadFruit = 0;
+    this.currentGoodFruit = 0;
+
   }
 
 }
@@ -38,19 +78,26 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor() {
+    var faktor = Math.floor(Math.random()*10);
+
+    if (faktor % 2 === 0) {
+      this.grade = 'good';
+    } else {
+      this.grade = 'bad';
+    }
   }
 }
 
-/**
-  * driver code untuk release 0
-  * let mangoTree = new MangoTree()
-  * do {
-  *   mangoTree.grow();
-  *   mangoTree.produceMangoes();
-  *   mangoTree.harverst();
-  *   console.log(`[Year ${tree._age} Report] Height = ${tree._height} | Fruits harvested = ${tree._harvested}`)
-  * } while (mangoTree.healthyStatus != false)
-  */
+
+   // driver code untuk release 0
+   let mangoTree = new MangoTree()
+   do {
+     mangoTree.grow();
+     mangoTree.produceMangoes();
+     mangoTree.harvest();
+     console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.tinggi} | Fruits harvested = ${mangoTree.harvested}\nKesehatan Pohon: ${mangoTree.healthStatus}`)
+  } while (mangoTree.healthyStatus !== false)
+
 
 // Release 1
 class AppleTree {}
